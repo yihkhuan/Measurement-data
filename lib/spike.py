52e4315b70f517dd9d54bdadf0679f0c37717333
Bryan Chong <yih00@graduate.utm.my>
2024-05-02 18:18:35 +0800

    updated library

--- lib/spike.py
+++ lib/spike.py
@@ -7,7 +7,12 @@ from scipy.stats import linregress
 import json
 
 GHZ_UNIT = 1e9
+MT_UNIT = 1e-3
+PLANK_CONSTANT = 6.626e-34
+BOHR_MAGNETON = 9.27e-24
 
+
+## General control functions
 def find_all(subname: str, path: str = "./") -> list:
   """
   Finds all folders containing the substring 'subname' at the given path.
@@ -155,3 +160,159 @@ def split_datablock(filename: str, delimiter: str = "") -> tuple[list, np.ndarra
   y = np.array(y_all)
 
   return name, x, y
+
+## ESR control functions
+def current_mag(current, magnet_name):
+    with open('./Analyse_data/current.json', 'r') as f:
+        fitted_current = json.load(f)
+
+    m = fitted_current[magnet_name].get('m')[0]
+    c = fitted_current[magnet_name].get('c')[0]
+    
+    return m * current + c 
+
+def mag_current(mag,magnet_name):
+    with open('./Analyse_data/current.json', 'r') as f:
+        fitted_current = json.load(f)
+
+    m = fitted_current[magnet_name].get('m')[0]
+    c = fitted_current[magnet_name].get('c')[0]
+    
+    return (mag - c)/m 
+
+def current_list(start: float = 0, end: float = 15):
+
+    currents = np.arange(start*100,end*100) / 100
+    current = []
+    for i in tqdm(currents):
+        finding = find("%.2fA.csv"%i,"./")
+        if finding is True:
+            current.append(i)
+        else:
+            pass
+    return current
+
+def esrcontour(magnet_name, fitting = False, xlim = (0,0), ylim = (0,0)):
+    current = current_list()
+    current = np.array(current)
+
+    freq, loss_0 = loss_list(current[0])
+    # magnetic = 19.713 * current[1:] + 224.92
+    magnetic = current_mag(current[1:],magnet_name)
+    B, F = np.meshgrid(magnetic,freq)
+    lossmesh = np.array([])
+    for i in current[1:]:
+        freq, loss = loss_list(i)
+        loss = loss_0 - loss
+        lossmesh = np.append(lossmesh,loss)
+
+    lossmesh = np.reshape(lossmesh,(-1,freq.shape[-1]))
+    ##Plotting
+    fig, ax = plt.subplots()
+    if fitting == True:
+        with open("./fitting_params.json","r") as f:
+            data = json.load(f)
+        
+        magnetic_field = np.array([])
+        resonance_freq = np.array([])
+        for current, obj in data.items():
+            mag = current_mag(float(current), magnet_name)
+            magnetic_field = np.append(magnetic_field,mag)
+        
+            frequency = obj["resonance-frequency"]
+            resonance_freq = np.append(resonance_freq,frequency)
+
+
+        m,c,*r = fit_linear(magnetic_field, resonance_freq)
+
+        mag_pts = np.arange(300,400)
+        freq_pts = mag_pts * m + c
+
+        ax.plot(magnetic_field,resonance_freq,"ko")
+        ax.plot(mag_pts, freq_pts, "k--", lw=2)
+
+    cp = ax.contourf(B, F, lossmesh.transpose(), cmap='rainbow', levels=100, zorder=1, vmin = 0)
+    cb = plt.colorbar(cp,orientation = 'vertical')
+    cb.ax.tick_params(labelsize = 16)
+    cb.set_label(label="absorption (dB)", fontsize=20)
+    if xlim != (0,0):
+        ax.set_xlim(xlim)
+    if ylim != (0,0):
+        ax.set_ylim(ylim)
+    ax.set_xlabel("magnetic field (mT)",fontsize=20)
+    ax.set_ylabel("frequency (GHz)",fontsize=20)
+    ax.tick_params(axis='both',which='major',labelsize=13)
+    ax.set_title("Contour plot of ESR signal", fontsize = 25)
+    # print(magnet_name)
+    mag2current = lambda x: mag_current(x, magnet_name)
+    current2mag = lambda x: current_mag(x , magnet_name)
+    # print(magnet_name)
+    secax = ax.secondary_xaxis('top', functions=(mag2current,current2mag))
+    secax.set_xlabel('current (A)',fontsize=20)
+    secax.tick_params(axis='x',which='major',labelsize=13)
+
+    # m = fitted_current[magnet_name].get('m')[0]
+    m,c,*r = fit_linear(magnetic_field, resonance_freq)
+
+    mag_pts = np.arange(300,400)
+    freq_pts = mag_pts * m + c
+
+    ax.plot(magnetic_field,resonance_freq,"ko")
+    ax.plot(mag_pts, freq_pts, "k--", lw=2)
+
+    ###True Values
+    with open("./Analyse_data/fitted.json","r") as g:
+        resonator = json.load(g)
+    
+    resonator_freq = resonator["fitted_data"][0].get('a')[0]
+
+    
+    expected_freq = lambda mag: mag * 2.0036 * BOHR_MAGNETON / PLANK_CONSTANT * mT / GHz
+    expected_mag = lambda freq: freq / (2.0036 * BOHR_MAGNETON / PLANK_CONSTANT * mT / GHz)
+    ax.plot(mag_pts, expected_freq(mag_pts), "r--", lw=2)
+    ax.plot(expected_mag(resonator_freq),resonator_freq,"ro")
+
+    plt.tight_layout()
+    # magnet_name = magnet_name.replace(magnet_name[-4:],"")
+    plt.savefig("./figs/ESR_contour_full_" + magnet_name + ".png")
+    pass
+
+def lorentz(x: np.ndarray, Al:float, b:float, a:float, offsetl:float) -> np.ndarray:
+    return Al / np.pi * (b / ((x - a)**2 + b**2)) + offsetl
+
+lorentz = lambda x, Al, b, a, offsetl: Al / np.pi * (b / ((x - a)**2 + b**2)) + offsetl
+
+def UTM_3D_plot(x: np.ndarray, y: np.ndarray, lossmesh: np.ndarray, ax, 
+                colourbar_label: str, xlabel: str, ylabel: str, title: str,
+                sec_xlabel: str, x2secx, secx2x, 
+                sec_ylabel: str, y2secy, secy2y,
+                ):
+    cp = ax.contourf(x, y, lossmesh.transpose(), cmap='rainbow', levels=100, zorder=1, vmin = 0)
+    cb = plt.colorbar(cp,orientation = 'vertical')
+    cb.ax.tick_params(labelsize = 16)
+    cb.set_label(label= colourbar_label, fontsize=20)
+
+    ax.set_xlabel(xlabel,fontsize=20)
+    ax.set_ylabel(ylabel,fontsize=20)
+    ax.tick_params(axis='both',which='major',labelsize=13)
+    ax.set_title(title, fontsize = 25)
+    
+
+
+    secax = ax.secondary_xaxis('top', functions=(x2secx,secx2x))
+    secax.set_xlabel('',fontsize=20)
+    secax.tick_params(axis='x',which='major',labelsize=13)
+
+    secay = ax.secondary_xaxis('right', functions=(y2secy,secy2y))
+    secax.set_xlabel('current (A)',fontsize=20)
+    secax.tick_params(axis='x',which='major',labelsize=13)
+
+def line_on_3D(fig, ax) -> tuple: 
+
+    pass
+
+self.magnet()
+self.mag_min()
+self.mag_max()
+self.freq_min()
+self.freq_max()
