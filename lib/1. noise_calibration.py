1f2e8363721bbea4c03298bed34999c0eec112cc
Bryan <120360739+yihkhuan@users.noreply.github.com>
2024-05-14 00:18:50 +0800

    lib update

--- lib/1. noise_calibration.py
+++ lib/1. noise_calibration.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from time import sleep
 from tqdm import tqdm
 import esr
-
+import datetime
 GHZ_UNIT = 1e9
 MHZ_UNIT = 1e6
 sa = SpectrumAnalyzer()
@@ -18,13 +18,22 @@ freq = sa.fetch_xarray()
 
 
 plt.figure()
+date_time = datetime.datetime.now()
+outputFile = f"{date_time.year}_{date_time.month}_{date_time.day}-{date_time.hour}{date_time.minute}"
+np.savetxt(outputFile, [])
+
+with open(outputFile, "ab") as output:
+    output.write(b"\n")
+    output.write(b"\n")
+    np.savetxt(output, freq.transpose(), header="ydata")
+output.close()
 # plt.plot(freq / GHZ_UNIT, spectrum)
 # # plt.savefig("./figs/Noise_calibration/before_zero.png")
 # plt.show()
 # plt.close()
 print(freq.shape, freq[0]/MHZ_UNIT, freq[-1]/MHZ_UNIT, (freq[1] - freq[0])/MHZ_UNIT)
 
-for i in tqdm(range(50)):
+for i in tqdm(range(100)):
     spectrum = sa.scan()
 
 sa.stor_thru(1)
@@ -50,23 +59,20 @@ sleep(5)
 
 not_0DB = False #Change after first time storing
 results = np.array([])
-t = np.ones(50)
+t = np.ones(100)
 stay = True
 while stay:
     name = float(input("Enter voltage: "))
     for i in tqdm(t):
-        if not_0DB:
-            sa.stor_thru()
-            sa.level(0.5)
-            not_0DB = False
-        elif results.size == 0:
+
+        if results.size == 0:
             results = np.append(results, sa.scan())
         else:
             results = np.vstack((results, sa.scan()))
         
 
     spectrum = np.average(results, axis=0)
-    freq = sa.fetch_xarray()
+    
 
     # if esr.find("csv", "./figs/Signal_detection/"):
     #     off_resonance = esr.find_all(".csv", "./figs/Signal_detection/")[0]
@@ -83,11 +89,16 @@ while stay:
 
     # print(spectrum.shape)
     # print(freq.shape)
-    output = np.vstack((freq, spectrum))
+    
     # print(output.transpose())
     # np.savetxt('./%s.csv'%name, output.transpose(), delimiter= ",")
-    np.savetxt(f"./figs/Signal_detection/{name}A.csv", output.transpose(), delimiter= ",")
-    sleep(5)
+
+    with open(outputFile, "ab") as output:
+        output.write(b"\n")
+        output.write(b"\n")
+        np.savetxt(output, spectrum.transpose(), header = name)
+    output.close()
+    
     try:
         stay = int(input("Do you want to stay (1/0): "))
     except:
